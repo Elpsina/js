9a5c1d43991854b6885a2c41e24b3e2b60002b68
Elpsina <pinaev.danila@mail.ru>
2019-02-03 02:39:46 +0300

    end work yeah

--- First2.js
+++ First2.js
@@ -1,20 +1,22 @@
-function Largest(){
-		if (arguments.length < 1)
-		return ("eror\n"); 
-		for (let a=arguments[0].length; a>0; a--){
-			for (let b=0; b<arguments[0].length-a; b++){
-				let reg = new RegExp(arguments[0].slice(b,b+a));
+function Largest(str){
+		for (let a=str[0].length; a=>0; a--){
+			for (let b=0; b<=str[0].length-a; b++){
+				let line = str[0].slice(b,b+a+1);
+				let reg = new RegExp(line);
 				let found = true;
-				for (let i=1; i<arguments.length; i++){
-					if(reg.test(arguments[i]))
+				for (let i=1; i<str.length; i++){
+					if(reg.test(str[i]))
 					continue;
 					found=false;
 					break;}
 					if(found)
-					return reg;		
+					return line;		
 			}
 		}
-		return "eror 404\n";
+		return "";
 		}
-	var sub=Largest("AbCqwe", "tyuAbC1234", "5678AbCyui");
-	console.log(sub + "\n");
+		if(process.argv.length <= 2)
+		console.log("");
+		else
+		console.log(Largest(process.argv.slice(2)));
+	
